c6d87f546bd045372f58914dca742a30fdec6766
tim-hilt <timhilt@live.de>
2019-10-11 17:51:30 +0200

    Began Package Management

--- init.el
+++ init.el
@@ -1,1 +1,13 @@
 (org-babel-load-file (concat user-emacs-directory "README.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(yasnippet-snippets yasnippet use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
