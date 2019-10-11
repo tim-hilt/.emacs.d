feef454352a6a9553e48d52627a880df405f5369
tim-hilt <timhilt@live.de>
2019-10-11 23:27:25 +0200

    Added Follow split, cleaned init.el

--- init.el
+++ init.el
@@ -4,12 +4,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(company-dabbrev-downcase nil)
- '(company-idle-delay 0.2)
- '(company-minimum-prefix-length 1)
- '(company-require-match nil)
- '(company-tooltip-align-annotations t)
  '(package-selected-packages
    '(magit dashboard doom-modeline all-the-icons counsel swiper doom-themes company-lsp lsp-mode company yasnippet-snippets yasnippet use-package)))
 (custom-set-faces
@@ -17,4 +11,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#1c1e1f" :foreground "#d6d6d4" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "PfEd" :family "Monospace")))))
