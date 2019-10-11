aab65dec42d18ce28233a0d910fee96c0cea337b
tim-hilt <timhilt@live.de>
2019-10-11 22:38:17 +0200

    Added a lot of packages

--- init.el
+++ init.el
@@ -4,8 +4,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(company-dabbrev-downcase nil)
+ '(company-idle-delay 0.2)
+ '(company-minimum-prefix-length 1)
+ '(company-require-match nil)
+ '(company-tooltip-align-annotations t)
  '(package-selected-packages
-   '(company-lsp lsp-mode company yasnippet-snippets yasnippet use-package)))
+   '(magit dashboard doom-modeline all-the-icons counsel swiper doom-themes company-lsp lsp-mode company yasnippet-snippets yasnippet use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
