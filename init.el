0200fcd0bc900045c0e747edd67f7e6692be1b12
Tim Hilt <timhilt@live.de>
2020-03-02 13:11:46 +0100

    More sensible window-splitting, auto-fill-mode and recentf sorting

--- init.el
+++ init.el
@@ -8,11 +8,12 @@
  '(company-tooltip-align-annotations t)
  '(package-selected-packages
    (quote
-    (pdf-tools ivy-rich hl-todo ivy-prescient prescient flycheck-popup-tip git-gutter-fringe fish-mode csv-mode json-mode yaml-mode disable-mouse which-key org-bullets sudo-edit hungry-delete rainbow-delimiters multiple-cursors expand-region visual-regexp-steroids visual-regexp toc-org org-toc-mode matlab-mode julia-mode lsp-ui company-box auctex magit dashboard doom-modeline all-the-icons counsel swiper doom-themes company-lsp lsp-mode company yasnippet-snippets yasnippet use-package)))
+    (adaptive-wrap sublimity centered-window ace-jump-mode pdf-tools ivy-rich hl-todo ivy-prescient prescient flycheck-popup-tip git-gutter-fringe fish-mode csv-mode json-mode yaml-mode disable-mouse which-key org-bullets sudo-edit hungry-delete rainbow-delimiters multiple-cursors expand-region visual-regexp-steroids visual-regexp toc-org org-toc-mode matlab-mode julia-mode lsp-ui company-box auctex magit dashboard doom-modeline all-the-icons counsel swiper doom-themes company-lsp lsp-mode company yasnippet-snippets yasnippet use-package)))
  '(use-package-always-defer t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#282c34" :foreground "#bbc2cf" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "SRC" :family "Source Code Pro")))))
+ '(default ((t (:inherit nil :stipple nil :background "#282c34" :foreground "#bbc2cf" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "SRC" :family "Source Code Pro"))))
+ '(fringe ((t (:background "#282c34")))))
