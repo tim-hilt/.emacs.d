1da2f6d63d29992bceeb750cf48d66d01a867839
Tim Hilt <timhilt@live.de>
2020-12-16 11:50:09 +0100

    Modified hl-todo-keywords

--- config.el
+++ config.el
@@ -153,7 +153,6 @@
         company-minimum-prefix-length 1
         company-global-modes '(not org-mode)))
 
-
 (use-package! ivy
   :defer t
   :bind
@@ -162,3 +161,32 @@
    ("TAB" . 'ivy-alt-done))
   :init
   (setq ivy-extra-directories nil))
+
+(use-package! hl-todo
+  :config
+  (global-hl-todo-mode))
+
+(after! hl-todo
+  (setq hl-todo-keyword-faces
+        `(;; For things that need to be done, just not today.
+          ("TODO" warning bold)
+          ;; For problems that will become bigger problems later if not
+          ;; fixed ASAP.
+          ("FIXME" error bold)
+          ;; For tidbits that are unconventional and not intended uses of the
+          ;; constituent parts, and may break in a future update.
+          ("HACK" font-lock-constant-face bold)
+          ;; For things that were done hastily and/or hasn't been thoroughly
+          ;; tested. It may not even be necessary!
+          ("REVIEW" font-lock-keyword-face bold)
+          ;; For especially important gotchas with a given implementation,
+          ;; directed at another user other than the author.
+          ("NOTE" success bold)
+          ;; For things that just gotta go and will soon be gone.
+          ("DEPRECATED" font-lock-doc-face bold)
+          ;; For a known bug that needs a workaround
+          ("BUG" error bold)
+          ;; For warning about a problematic or misguiding code
+          ("XXX" font-lock-constant-face bold)
+          ;; For something, that's finally finished
+          ("DONE" success bold))))
